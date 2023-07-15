f72b4135a729d92db51bce5790f9c4d8a2ce2c6d
Adrian Baudat <granbaud@hotmail.com>
2023-07-15 09:52:10 +0200

    feat: add tooltips to edit mode

---
+++ Assets/EditTooltipManager.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class EditTooltipManager : MonoBehaviour
+{
+    public GameObject editTooltip;
+    public Image editTooltipImage;
+    public TMP_Text editTooltipTitle, editTooltipDescription;
+    public RectTransform mainCanvas;
+
+    public void PopulateTooltip(Sprite sprite, string title, string description)
+    {
+        editTooltipImage.sprite = sprite;
+        editTooltipTitle.text = title;
+        editTooltipDescription.text = description;
+    }
+
+    public void DisplayTooltip(RectTransform element)
+    {
+        editTooltip.SetActive(true);
+        editTooltip.transform.position = ComputeTooltipPosition(element);
+    }
+
+    public void HideTooltip()
+    {
+        editTooltip.SetActive(false);
+    }
+
+    private Vector2 ComputeTooltipPosition(RectTransform elementRectTransform)
+    {
+        RectTransform thisRectTransform = editTooltip.GetComponent<RectTransform>();
+        Vector2 topLeftCornerOfElement = new Vector2(elementRectTransform.position.x - elementRectTransform.rect.width / 2, elementRectTransform.position.y + elementRectTransform.rect.height / 2);
+        Vector2 idealTooltipPosition = new Vector2(topLeftCornerOfElement.x + thisRectTransform.rect.width / 2, topLeftCornerOfElement.y + thisRectTransform.rect.height / 2);
+        Vector2 clampedTooltipPosition = KeepFullyOnScreen(idealTooltipPosition, thisRectTransform);
+        return clampedTooltipPosition;
+    }
+
+    private Vector2 KeepFullyOnScreen(Vector2 newPos, RectTransform thisRectTransform)
+    {
+        float minX = (mainCanvas.sizeDelta.x - thisRectTransform.sizeDelta.x) * -0.5f;
+        float maxX = (mainCanvas.sizeDelta.x - thisRectTransform.sizeDelta.x) * 0.5f;
+        float minY = (mainCanvas.sizeDelta.y - thisRectTransform.sizeDelta.y) * -0.5f;
+        float maxY = (mainCanvas.sizeDelta.y - thisRectTransform.sizeDelta.y) * 0.5f;
+
+        newPos.x = Mathf.Clamp(newPos.x, minX, maxX);
+        newPos.y = Mathf.Clamp(newPos.y, minY, maxY);
+
+        return newPos;
+    }
+
+}
--- Assets/UiBuildElement.cs
+++ Assets/UiBuildElement.cs
@@ -1,24 +1,40 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using TMPro;
 
-public class UiBuildElement : MonoBehaviour, IPointerDownHandler
+public class UiBuildElement : MonoBehaviour, IPointerDownHandler, IPointerEnterHandler, IPointerExitHandler
 {
     [SerializeField]
     private int elementPrefabIndex;
+    [SerializeField]
+    private string elementTitle, elementDescription;
     private CubeGridEditor cubeGridEditor;
-    private Image image;
     private SoundManager soundManager;
     private TutorialManager tutorialManager;
+    private EditTooltipManager editTooltipManager;
+    private RectTransform thisRectTransform;
+
+    private static readonly float TOOLTIP_DISPLAY_HOVER_TIME_SECONDS = 1;
+
+    private Sprite previewSprite;
+    private float hoverTimeSeconds = 0;
+    private bool isHovering = false;
 
     private void Awake()
     {
         cubeGridEditor = FindObjectOfType<CubeGridEditor>();
         soundManager = FindObjectOfType<SoundManager>();
         tutorialManager = FindObjectOfType<TutorialManager>();
-        image = GetComponent<Image>();
-        RuntimePreviewGenerator.GenerateModelPreviewAsync(tex => image.sprite = Sprite.Create(tex, new Rect(0.0f, 0.0f, tex.width, tex.height), new Vector2(0.5f, 0.5f), 100.0f), 
-            PrefabHelper.PrefabFromIndex(elementPrefabIndex).transform);
+        editTooltipManager = FindObjectOfType<EditTooltipManager>();
+        thisRectTransform = GetComponent<RectTransform>();
+        Image image = GetComponentInChildren<Image>();
+        RuntimePreviewGenerator.GenerateModelPreviewAsync(tex => {
+            previewSprite = Sprite.Create(tex, new Rect(0.0f, 0.0f, tex.width, tex.height), new Vector2(0.5f, 0.5f), 100.0f);
+            image.sprite = previewSprite;
+        }, PrefabHelper.PrefabFromIndex(elementPrefabIndex).transform);
+        TMP_Text title = GetComponentInChildren<TMP_Text>();
+        title.SetText(elementTitle);
     }
 
     public void OnPointerDown(PointerEventData eventData)
@@ -26,5 +42,30 @@ public class UiBuildElement : MonoBehaviour, IPointerDownHandler
         cubeGridEditor.ChangeCurrentPrefabIndex(elementPrefabIndex);
         soundManager.PlaySelectClip();
         tutorialManager.SelectBlock();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        isHovering = true;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        isHovering = false;
+        hoverTimeSeconds = 0;
+        editTooltipManager.HideTooltip();
+    }
+
+    private void Update()
+    {
+        if (isHovering)
+        {
+            hoverTimeSeconds += Time.deltaTime;
+        }
+        if (hoverTimeSeconds > TOOLTIP_DISPLAY_HOVER_TIME_SECONDS)
+        {
+            editTooltipManager.PopulateTooltip(previewSprite, elementTitle, elementDescription);
+            editTooltipManager.DisplayTooltip(thisRectTransform);
+        }
     }
 }
